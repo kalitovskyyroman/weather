97d94bcf1ced3225cffd57bc957b1be98d7ef9e8
Roman Kalitovskyy <Roman Kalitovksyy>
2022-01-25 09:40:23 +0200

    redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
 
-const App = () => <div className="App">api1: {process.env.REACT_APP_API_KEY}</div>;
+const App = () => <div className="App">api2: {process.env.REACT_APP_API_KEY}</div>;
 
 export default App;
